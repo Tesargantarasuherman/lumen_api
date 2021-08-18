186586538101f6dd6a8d71eccf8c7d9e1473c560
Iamsuherman <tgantara@gmail.com>
2021-08-18 23:22:22 +0700

    update validasi pertandingan

--- app/Http/Controllers/PertandinganController.php
+++ app/Http/Controllers/PertandinganController.php
@@ -12,45 +12,72 @@ use App\Klasemen;
 
 class PertandinganController extends BaseController
 {
-
-
     public function tambahPertandingan(Request $request)
     {
         $klub_home = $request->input('klub_home');
         $klub_away = $request->input('klub_away');
-        $waktu_pertandingan = $request->input('waktu_pertandingan');
-        $this->validate($request, [
-            'klub_home' => 'required',
-            'klub_away' => 'required',
-            'waktu_pertandingan' => 'required',
-        ]);
-        $pertandingan = Pertandingan::create([
-            'klub_home' => $klub_home,
-            'klub_away' => $klub_away,
-            'waktu_pertandingan' => $waktu_pertandingan,
-        ]);
 
-        if($pertandingan)
-        {
-            return response()->json([
-                'success' => true,
-                'message' => 'Pertandingan sukses di buat',
-                'data'    => $pertandingan
-            ],201);
+        $data_home_klub = Klasemen::where('id', $klub_home)->first();
+        $data_away_klub = Klasemen::where('id', $klub_away)->first();
+
+        if($klub_home == $klub_away){
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'id tidak home dan away boleh sama',
+                    'data' => '',
+                ],
+                400
+            );
         }
-        else
-        {
-            return response()->json([
-                'success' => false,
-                'message' => 'Pertandingan gagal di buat',
-                'data'    => ''
-            ],400);
+        if ($data_home_klub['id_turnamen'] == $data_away_klub['id_turnamen']) {
+            $waktu_pertandingan = $request->input('waktu_pertandingan');
+            $this->validate($request, [
+                'klub_home' => 'required',
+                'klub_away' => 'required',
+                'waktu_pertandingan' => 'required',
+            ]);
+            $pertandingan = Pertandingan::create([
+                'klub_home' => $klub_home,
+                'klub_away' => $klub_away,
+                'waktu_pertandingan' => $waktu_pertandingan,
+            ]);
+
+            if ($pertandingan) {
+                return response()->json(
+                    [
+                        'success' => true,
+                        'message' => 'Pertandingan sukses di buat',
+                        'data' => $pertandingan,
+                    ],
+                    201
+                );
+            } else {
+                return response()->json(
+                    [
+                        'success' => false,
+                        'message' => 'Pertandingan gagal di buat',
+                        'data' => '',
+                    ],
+                    400
+                );
+            }
+        }
+        else{
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'id tidak dalam turnamen yang sama',
+                    'data' => '',
+                ],
+                400
+            );
         }
     }
 
-    public function updatePertandingan(Request $request,$id)
+    public function updatePertandingan(Request $request, $id)
     {
-        $data_pertandingan = Pertandingan::where('id',$id)->first();
+        $data_pertandingan = Pertandingan::where('id', $id)->first();
 
         $skor_home = $request->input('skor_home');
         $skor_away = $request->input('skor_away');
@@ -59,92 +86,114 @@ class PertandinganController extends BaseController
             'skor_away' => 'required',
         ]);
 
-
         // cek skor
-        if($data_pertandingan['status'] != 1 ){
+        if ($data_pertandingan['status'] != 1) {
             // ambil data klasemen
-            $data_home_klub = Klasemen::where('id',$data_pertandingan['klub_home'])->first();
-            $data_away_klub = Klasemen::where('id',$data_pertandingan['klub_away'])->first();
+            $data_home_klub = Klasemen::where(
+                'id',
+                $data_pertandingan['klub_home']
+            )->first();
+            $data_away_klub = Klasemen::where(
+                'id',
+                $data_pertandingan['klub_away']
+            )->first();
 
             // tim kandang menang
-            if($skor_home > $skor_away){
-
-                $home_klub = Klasemen::where('id',$data_pertandingan['klub_home'])->update([
-                    'poin'     => $data_home_klub['poin'] + 3,
-                    'main'     => $data_home_klub['main'] + 1,
-                    'menang'     => $data_home_klub['menang'] + 1,
+            if ($skor_home > $skor_away) {
+                $home_klub = Klasemen::where(
+                    'id',
+                    $data_pertandingan['klub_home']
+                )->update([
+                    'poin' => $data_home_klub['poin'] + 3,
+                    'main' => $data_home_klub['main'] + 1,
+                    'menang' => $data_home_klub['menang'] + 1,
                 ]);
-                $away_klub = Klasemen::where('id',$data_pertandingan['klub_away'])->update([
-                    'kalah'     => $data_away_klub['kalah'] + 1,
-                    'main'     => $data_away_klub['main'] + 1,
+                $away_klub = Klasemen::where(
+                    'id',
+                    $data_pertandingan['klub_away']
+                )->update([
+                    'kalah' => $data_away_klub['kalah'] + 1,
+                    'main' => $data_away_klub['main'] + 1,
                 ]);
             }
             // tim tamu menang
-            else if($skor_home < $skor_away){
-    
-                $away_klub = Klasemen::where('id',$data_pertandingan['klub_away'])->update([
-                    'poin'     => $data_away_klub['poin'] + 3,
-                    'main'     => $data_away_klub['main'] + 1,
-                    'menang'     => $data_away_klub['menang'] + 1,
+            elseif ($skor_home < $skor_away) {
+                $away_klub = Klasemen::where(
+                    'id',
+                    $data_pertandingan['klub_away']
+                )->update([
+                    'poin' => $data_away_klub['poin'] + 3,
+                    'main' => $data_away_klub['main'] + 1,
+                    'menang' => $data_away_klub['menang'] + 1,
                 ]);
 
-                $home_klub = Klasemen::where('id',$data_pertandingan['klub_home'])->update([
-                    'kalah'     => $data_home_klub['kalah'] + 1,
-                    'main'     => $data_home_klub['main'] + 1,
+                $home_klub = Klasemen::where(
+                    'id',
+                    $data_pertandingan['klub_home']
+                )->update([
+                    'kalah' => $data_home_klub['kalah'] + 1,
+                    'main' => $data_home_klub['main'] + 1,
                 ]);
             }
             // imbang
-            else if($skor_home == $skor_away){
-                $away_klub = Klasemen::where('id',$data_pertandingan['klub_away'])->update([
-                    'poin'     => $data_away_klub['poin'] + 1,
-                    'main'     => $data_away_klub['main'] + 1,
-                    'imbang'     => $data_away_klub['imbang'] + 1,
+            elseif ($skor_home == $skor_away) {
+                $away_klub = Klasemen::where(
+                    'id',
+                    $data_pertandingan['klub_away']
+                )->update([
+                    'poin' => $data_away_klub['poin'] + 1,
+                    'main' => $data_away_klub['main'] + 1,
+                    'imbang' => $data_away_klub['imbang'] + 1,
                 ]);
 
-                $home_klub = Klasemen::where('id',$data_pertandingan['klub_home'])->update([
-                    'poin'     => $data_home_klub['poin'] + 1,
-                    'main'     => $data_home_klub['main'] + 1,
-                    'imbang'     => $data_home_klub['imbang'] + 1,
+                $home_klub = Klasemen::where(
+                    'id',
+                    $data_pertandingan['klub_home']
+                )->update([
+                    'poin' => $data_home_klub['poin'] + 1,
+                    'main' => $data_home_klub['main'] + 1,
+                    'imbang' => $data_home_klub['imbang'] + 1,
                 ]);
-
             }
-            // end update point           
-        }
-        else{
-            return response()->json([
-                'success' => false,
-                'message' => 'Pertandingan telah selesai',
-                'data'    => $data_pertandingan
-            ],201);
+            // end update point
+        } else {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Pertandingan telah selesai',
+                    'data' => $data_pertandingan,
+                ],
+                201
+            );
         }
 
         // end cek skor
 
-
         // end update klasemen
         $pertandingan = Pertandingan::whereId($id)->update([
-            'skor_home'     => $skor_home,
-            'skor_away'   => $skor_away,
-            'status'   => 1,
+            'skor_home' => $skor_home,
+            'skor_away' => $skor_away,
+            'status' => 1,
         ]);
 
-
-
-        if($pertandingan)
-        {
-            return response()->json([
-                'success' => true,
-                'message' => 'Pertandingan sukses di buat',
-                'data'    => $pertandingan
-            ],201);
-        }
-        else
-        {
-            return response()->json([
-                'success' => false,
-                'message' => 'Pertandingan gagal di buat',
-                'data'    => ''
-            ],400);
+        if ($pertandingan) {
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Pertandingan sukses di buat',
+                    'data' => $pertandingan,
+                ],
+                201
+            );
+        } else {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Pertandingan gagal di buat',
+                    'data' => '',
+                ],
+                400
+            );
         }
     }
 }
