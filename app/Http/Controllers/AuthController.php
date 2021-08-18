d974e368ac10bd4ecdabf1396d0d64f97ec53fc9
Iamsuherman <tgantara@gmail.com>
2021-08-18 09:42:00 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\User;
+
+class AuthController extends BaseController
+{
+    public function register(Request $request)
+    {
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = Hash::make($request->input('password'));
+
+        $register = User::create([
+            'name' => $name,
+            'email'=> $email,
+            'password'=> $password,
+        ]);
+
+        if($register)
+        {
+            return response()->json([
+                'success' => true,
+                'message' => 'Register Succes',
+                'data'    => $register
+            ],201);
+        }
+        else
+        {
+            return response()->json([
+                'success' => false,
+                'message' => 'Register Fail!',
+                'data'    => ''
+            ],400);
+        }
+
+    }
+    public function login(Request $request)
+    {
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+
+<?php
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+// Generate Aplication Key
+
+$router->get('/key',function (){
+    return Str::random(32);
+});
+
+$router->get('/foo',function (){
+    return 'Hello, GET method';
+});
+
+// name alias
+$router->get('/profile',['as'=>'route.profile',function(){
+    return route('route.profile');
+}]);
+// end nama Alias
+// mengelompokkan route
+$router->group(['prefix' => 'user'],function()use ($router){
+    $router->get('home',['middleware' => 'age'],function(){
+        return 'Home User' ;
+    });
+
+    $router->get('profile',function(){
+        return 'Profile User' ;
+    });
+});
+
+// end mengelompokan route
+// middelware 
+$router->get('/admin/home',['middleware' => 'age',function(){
+    return 'Home Admin' ;
+}]);
+$router->get('/fail',function(){
+    return 'fail' ;
+});
+
+// end middleware
+
+// AUTH
+$router->post('/register','AuthController@register');
+$router->post('/login','AuthController@login');
