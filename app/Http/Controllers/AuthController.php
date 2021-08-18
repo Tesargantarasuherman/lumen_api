30c0f8e34d2c79be60c8b242e7121fd07e9f75da
Iamsuherman <tgantara@gmail.com>
2021-08-18 09:53:06 +0700

    create login logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 use App\User;
 
@@ -43,5 +44,32 @@ class AuthController extends BaseController
     public function login(Request $request)
     {
         $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = User::where('email',$email)->first();
+
+        if(Hash::check($password,$user->password)){
+            $apiToken = base64_encode(Str::random(40));
+
+            $user->update([
+                'api_token' => $apiToken
+            ]);
+            return response()->json([
+                'success' => true,
+                'message' => 'Login Succes',
+                'data'    =>      [
+                    'user'      => $user,
+                    'api_token' => $apiToken
+                ]          
+                ],201);
+        }
+        else
+        {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Fail!',
+                'data'    =>  ''       
+                ]);
+        }
     }
 }
