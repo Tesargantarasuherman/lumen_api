5417ba3a3e6adeb146f05d50cca6c32924ca8ae6
Iamsuherman <tgantara@gmail.com>
2021-08-18 10:07:35 +0700

    create logic user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,21 @@ class UserController extends BaseController
 
     public function show($id)
     {
+        $user = User::find($id);
 
+        if($user){
+            return response()->json([
+                'success' => true,
+                'message' => 'User Found',
+                'data' => $user
+            ],200);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'message' => 'User Not Found',
+                'data' => ''
+            ],404);
+        }
     }
 }
